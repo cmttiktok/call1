45a20a1e54e7631daa839601edabf4ff27801806
cmttiktok <phamanhtu535@gmail.com>
2026-04-19 19:59:27 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').Server(app);
+
+// Cho phép truy cập file index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Cấu hình Port cho Render
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Hệ thống giao diện đang chạy trên Render tại port: ${PORT}`);
+});
