f84adfbed15f431a52a43c1cb592291106deb45d
cmttiktok <phamanhtu535@gmail.com>
2026-06-13 23:04:19 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+    console.log(`Giao diện Render đang chạy trên port ${PORT}`);
+});
